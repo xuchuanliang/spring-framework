d86bb9d9d9bbf875ee4ca4e4169d4f6af03eb350
xuchuanliang361@163.com <xuchuanliang361@163.com>
2020-06-12 11:12:20 +0800

    test

--- spring-context/src/main/java/org/springframework/context/support/ClassPathXmlApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/support/ClassPathXmlApplicationContext.java
@@ -141,6 +141,7 @@ public class ClassPathXmlApplicationContext extends AbstractXmlApplicationContex
 		super(parent);
 		setConfigLocations(configLocations);
 		if (refresh) {
+			//important method!
 			refresh();
 		}
 	}
