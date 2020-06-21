e6441b766ecc153055f1d52d698f036d59f9d839
ant <xuchuanliang361@163.com>
2020-06-21 20:06:27 +0800

    commit

---
+++ mySpring/src/main/java/com/ant/TestMain.java
@@ -0,0 +1,9 @@
+package com.ant;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestMain {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext();
+	}
+}
